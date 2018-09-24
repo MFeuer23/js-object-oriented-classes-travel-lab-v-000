bbd1777928f060dba5fcd5411bc89e67251943aa
Mallory Feuer <mallory.feuer@gmail.com>
2018-09-24 19:27:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ class Driver {
   }
 }
 
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+
 class Route {
   constructor(beginningLocation, endingLocation) {
     this.beginningLocation = beginningLocation;
