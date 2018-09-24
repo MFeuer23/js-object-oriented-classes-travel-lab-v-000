3527b53c5cbf949423e0879d516e8544bc6bcef9
Mallory Feuer <mallory.feuer@gmail.com>
2018-09-24 19:24:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ class Driver {
     return year - (this.startDate.getFullYear() + Math.round(this.startDate.getMonth()/12));
   }
 }
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+}
