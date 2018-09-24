54e187be2b8adb5f3be39f68553591f9091542cd
Mallory Feuer <mallory.feuer@gmail.com>
2018-09-24 19:20:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ class Driver {
   }
   
   yearsExperienceFromBeginningOf(year) {
-    return year - Math.round(this.startDate.getMonth()/12)
+    return year - this.startDate.setFullYears(this.startDate.getFullYear + Math.round(this.startDate.getMonth()/12))
   }
 }
